3f828559858f0ff0a2ceb26a86ca749e28332d78
Daniel Monteiro <158519099+danielMonteiro84@users.noreply.github.com>
2024-03-24 17:53:10 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return <h1>
+        Esse é meu primeiro deploy em uma hospedagem remota e real, e eu não poderia deixar de agradecer todo apoio que vc tem me dado. Amo muito voce!!!
+    </h1>
+}
+
+export default Home;
